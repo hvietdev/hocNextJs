d4c647172899b9a3058b69e1d26059ce82e111e5
Việt Hoàng <vietcntin@gmail.com>
2025-04-24 10:03:04 +0700

    Fist change

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+// import form  font gogole
+import { Inter, Lusitana } from 'next/font/google';
+// Export font to use in the app
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });

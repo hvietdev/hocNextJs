fce4767703c7889dce101a48fac9c4b0f2cbd2c8
Việt Hoàng <vietcntin@gmail.com>
2025-04-24 20:46:54 +0700

    Add server validator

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,5 +2,7 @@ import { fetchRevenue } from "@/app/lib/data";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
+  console.log("v:", revenue);
+  
    return <p>Customers Page</p>
  }
